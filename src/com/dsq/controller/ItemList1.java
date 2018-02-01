d9e5eabdd9ea989b7376ba7ca5035fd3946ff0f7
dongsijeong <18640947990@163.com>
2018-02-01 17:00:37 +0800

    second commit

---
+++ src/com/dsq/controller/ItemList1.java
@@ -0,0 +1,20 @@
+package com.dsq.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ItemList1 implements Controller {
+
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+
+        //商品列表
+        //List<Items>
+
+        return null;
+    }
+}
